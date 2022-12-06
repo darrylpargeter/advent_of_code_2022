2b1853337964d7f093ee364e6ee36980bba5469a
Darryl Pargeter <darryl@pargeter.dev>
2022-12-06 07:32:40 +0000

    day6 refactor

--- day6/puzzle1.mjs
+++ day6/puzzle1.mjs
@@ -1,25 +1,19 @@
 import { getInput, split, slice, map } from '../utils/index.mjs';
 
-const testData = [
-    'bvwbjplbgvbhsrlpgdmjqwftvncz',
-    'nppdvjthqldpwncqszvftbrmjlhg',
-    'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg',
-    'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw',
-];
-
 const data = await getInput('./day6/input.txt');
 const input = split('')(data.trim());
 
-function window(arr, idx=0, size) {
-    if (idx === arr.length) return output;
+function window(arr, size, idx=0) {
+    if (idx === arr.length) return;
+
     const [extract] = slice(arr, idx, idx + size);
     const set = new Set(extract);
     if (set.size === size) {
         return { idx, size: idx + size };
     }
 
-    return window(arr, idx + 1, size);
+    return window(arr, size, idx + 1);
 }
 
-const output = window(input, 0, 4);
+const output = window(input, 4);
 console.log(output)
